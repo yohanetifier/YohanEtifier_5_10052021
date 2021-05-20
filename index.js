1971501114cea4dda6cae261327e6fdc24b68851
Yohan Etifier <etifier.y@gmail.com>
2021-05-20 20:12:52 +0200

    confirm page

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+/* -------------------- Index ---------------  */
+function searchBar(){
+  
+}
 
 function getProduct() {
   for (let i = 0; i < 5; i++) {
@@ -32,13 +36,8 @@ function getProduct() {
         cardBody.appendChild(description);
         link.className = "btn btn-primary";
         link.textContent = "pour plus de details";
-        link.setAttribute("href", "productpage.html?id=" + value[i]._id); 
+        link.setAttribute("href", "productpage.html?id=" + value[i]._id);
         cardBody.appendChild(link);
-        link.addEventListener("click", function () {
-          let paramsString = "productpage.html"
-          let url = new URLSearchParams(paramsString); 
-          url.append("id", value[i]._id)
-        });
       })
       .catch(function (err) {
         console.log("une erreur est survenue");
@@ -46,146 +45,188 @@ function getProduct() {
   }
 }
 
-
-
-
 /*---------------------------------- Product page---------------------------- */
 
 /* ---------Get data--------- */
 
-function productPage(){ 
+function productPage() {
   fetch(`http://localhost:3000/api/cameras/${location.search.substring(4)}`)
-  .then(function(response){
-    if (response.ok){
-      return response.json()
-    }
-  }).then(function(value){
+    .then(function (response) {
+      if (response.ok) {
+        return response.json();
+      }
+    })
+    .then(function (value) {
+      /* Selection element of the DOM */
 
-    /* Selection element of the DOM */
+      let img = document.querySelector(".card-img-top");
+      let heading = document.querySelector(".card-title");
+      let description = document.querySelector(".card-text");
+      let price = document.querySelector(".price");
+      let dropdownlist = document.querySelector(".form-select");
+      let addToBasket = document.querySelector(".btn");
 
-    let img = document.querySelector(".card-img-top")
-    let heading = document.querySelector(".card-title")
-    let description = document.querySelector(".card-text")
-    let price = document.querySelector(".price")
-    let dropdownlist = document.querySelector(".form-select")
-    let addToBasket = document.querySelector(".btn"); 
+      /* Set up Element of the DOM */
 
+      img.setAttribute("src", value.imageUrl);
+      heading.textContent = value.name;
+      description.textContent = value.description;
+      price.textContent = `${value.price}€`;
+      quantity.addEventListener("input", function (e) {
+        price.textContent = value.price * e.target.value + "€";
+      });
+      for (let i = 0; i < value.lenses.length; i++) {
+        let option = document.createElement("option");
+        dropdownlist.appendChild(option);
+        option.setAttribute("value", value.lenses[i]);
+        option.textContent = value.lenses[i];
+      }
 
-    /* Set up Element of the DOM */
+      /* --Add to basket-- */
 
-    img.setAttribute("src", value.imageUrl)
-    heading.textContent = value.name
-    description.textContent = value.description;
-    price.textContent = `${value.price}€`
-    quantity.addEventListener("input", function(e){
-      price.textContent = value.price * e.target.value + "€" 
+      addToBasket.addEventListener("click", function () {
+        let price = document.querySelector(".price");
+        let quantity = document.getElementById("quantity").value;
+        let option =
+          document.querySelector(".form-select").options[
+            document.querySelector(".form-select").selectedIndex
+          ].text;
 
-  })
-  for (let i = 0; i < value.lenses.length; i++){
-    let option = document.createElement("option"); 
-    dropdownlist.appendChild(option); 
-    option.setAttribute("value", value.lenses[i]); 
-    option.textContent = value.lenses[i]; 
+        let selectionClient = {
+          id: value._id,
+          name: value.name,
+          option: option,
+          quantity: quantity,
+          price: value.price * quantity + "€",
+        };
 
-  }
+        /* Setup POPup */
 
- 
+        const popup = () => {
+          if (
+            window.confirm(
+              `Vous avez ajouté au panier  ${selectionClient.quantity} ${selectionClient.name} avec une lentille de ${selectionClient.option} pour un total de ${selectionClient.price}. Voulez vous retouner sur la page d'accueil ou voir votre panier? OK pour voir votre panier, ANNULER pour retourner sur la page d'accueil `
+            )
+          ) {
+            window.open("panier.html");
+          } else {
+            window.open("index.html");
+          }
+        };
 
-  /* --Add to basket-- */
+        let shoppingCart = JSON.parse(localStorage.getItem("product"));
+        /* Product in the Local Storage or not  */
 
-  addToBasket.addEventListener("click", function(){
-  let price = document.querySelector(".price")
-  let quantity  = document.getElementById("quantity").value
-  let option = document.querySelector(".form-select").options[document.querySelector(".form-select").selectedIndex].text
-  
-  let selectionClient = {
-    name: value.name, 
-    option: option, 
-    quantity: quantity, 
-    price: value.price * quantity + "€",
-  }
-
-  /* Setup POPup */
-
-  const popup = () => {
-    if (window.confirm(`Vous avez ajouté au panier  ${selectionClient.quantity} ${selectionClient.name} avec une lentille de ${selectionClient.option} pour un total de ${selectionClient.price}. Voulez vous retouner sur la page d'accueil ou voir votre panier? OK pour voir votre panier, ANNULER pour retourner sur la page d'accueil `)) {
-      window.open("panier.html");
-  }else {
-    window.open("index.html");
-  }
-  }
-
-  let shoppingCart = JSON.parse(localStorage.getItem("product"))
-  /* Product in the Local Storage or not  */
-  
-  
-  if (shoppingCart){
-    shoppingCart.push(selectionClient); 
-    localStorage.setItem("product", JSON.stringify(shoppingCart))
-    popup()
-  }else {
-    shoppingCart = []; 
-    shoppingCart.push(selectionClient); 
-    localStorage.setItem("product", JSON.stringify(shoppingCart))
-    popup()
-  }
-  })
-
-  }).catch(function(error){
-    console.log("une erreur est survenue")
-  })
+        if (shoppingCart) {
+          shoppingCart.push(selectionClient);
+          localStorage.setItem("product", JSON.stringify(shoppingCart));
+          popup();
+        } else {
+          shoppingCart = [];
+          shoppingCart.push(selectionClient);
+          localStorage.setItem("product", JSON.stringify(shoppingCart));
+          popup();
+        }
+      });
+    })
+    .catch(function (error) {
+      console.log("une erreur est survenue");
+    });
 }
-
-
 
 /* -------------------------  Basket page------------------------------------------------ */
 
-function basket(){
-
+function basket() {
   /* Create element of the DOM */
 
-  if (localStorage.product){
-    for (let i = 0; i < JSON.parse(localStorage.product).length; i++){
-      let list = JSON.parse(localStorage.getItem("product"))
-      let tbody = document.querySelector(".tbody")
-      let rank = document.createElement("tr"); 
-      let cellPrice = document.createElement("td")
-      let cellTitle = document.createElement("td"); 
-      let cellQuantity = document.createElement("td"); 
-      let cellOption = document.createElement("td"); 
-      let total = document.querySelector(".total")
-  
-  
-    /* Add Attribute */
-  
-      cellPrice.className = "price"
-      cellTitle.className = "title"
-      cellOption.className = "option"
-      cellQuantity.className = "quantity"
-  
-    /* Insertion in the DOM */
+  if (localStorage.product) {
+    for (let i = 0; i < JSON.parse(localStorage.product).length; i++) {
+      let list = JSON.parse(localStorage.getItem("product"));
+      let tbody = document.querySelector(".tbody");
+      let rank = document.createElement("tr");
+      let cellPrice = document.createElement("td");
+      let cellTitle = document.createElement("td");
+      let cellQuantity = document.createElement("td");
+      let cellOption = document.createElement("td");
+      let total = document.querySelector(".total");
       
-    tbody.appendChild(rank)
-    rank.appendChild(cellTitle)
-    rank.appendChild(cellQuantity)
-    rank.appendChild(cellOption)
-    rank.appendChild(cellPrice)
-  
-    /* Content of the node */
-  
-    cellTitle.textContent  = list[i].name; 
-    cellQuantity.textContent = list[i].quantity; 
-    cellPrice.textContent = list[i].price ; 
-    cellOption.textContent = list[i].option; 
-    total.textContent = list[i].price 
-  
-  } 
+
+      /* Add Attribute */
+
+      cellPrice.className = "price";
+      cellTitle.className = "title";
+      cellOption.className = "option";
+      cellQuantity.className = "quantity";
+      
+
+      /* Insertion in the DOM */
+
+      tbody.appendChild(rank);
+      rank.appendChild(cellTitle);
+      rank.appendChild(cellQuantity);
+      rank.appendChild(cellOption);
+      rank.appendChild(cellPrice);
+
+      /* Content of the node */
+
+      cellTitle.textContent = list[i].name;
+      cellQuantity.textContent = list[i].quantity;
+      cellPrice.textContent = list[i].price;
+      cellOption.textContent = list[i].option;
+      total.textContent = list[i].price;
     }
-  }
-      
+
+    /*    Send data to server   */
+
+    let submit = document.getElementById("submit");
+    let firstName = document.getElementById("inputFirstName");
+    let lastName = document.getElementById("inputLastName");
+    let email = document.getElementById("inputEmail");
+    let address = document.getElementById("inputAddress");
+    let city = document.getElementById("inputCity");
+    let test = JSON.parse(localStorage.getItem("product"));
     
 
+    submit.addEventListener("click", function () {
+      const request = {
+        contact: {
+          firstName: firstName.value,
+          lastName: lastName.value,
+          address: address.value,
+          city: city.value,
+          email: email.value,
+        },
+        products: [],
+      };
+      for (let i = 0; i < test.length; i++) {
+        let id = JSON.parse(localStorage.getItem("product"))[i].id;
+        request.products.push(id);
+      }
 
+      fetch("http://localhost:3000/api/cameras/order/", {
+        method: "POST",
+        body: JSON.stringify(request),
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      })
+        .then(function (response) {
+          if (response.ok) {
+            return response.json();
+          }
+        })
+        .then((value) => {
+          submit.setAttribute("href", "confirm.html?order=" + value.orderId); 
+        });
+    });
+  }
+}
 
+/* ------------------Confirm Order ------------------- */
 
-
+function confirmOrder(){
+  let confirm = document.querySelector(".confirm"); 
+  confirm.textContent = `Votre commande n° ${location.search.substring(7)} a bien été validé. Vous recevrez un mail de confirmation dans quelques instants`
+  localStorage.clear();
+}

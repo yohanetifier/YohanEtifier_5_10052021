686262acb726acf20a701351fee98fc0ba28ca7e
Yohan Etifier <etifier.y@gmail.com>
2021-05-26 13:18:29 +0200

    form section

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 /* -------------------- Index ---------------  */
-function searchBar(){
-  
-}
 
 function getProduct() {
   for (let i = 0; i < 5; i++) {
@@ -100,32 +97,17 @@ function productPage() {
           price: value.price * quantity + "€",
         };
 
-        /* Setup POPup */
-
-        const popup = () => {
-          if (
-            window.confirm(
-              `Vous avez ajouté au panier  ${selectionClient.quantity} ${selectionClient.name} avec une lentille de ${selectionClient.option} pour un total de ${selectionClient.price}. Voulez vous retouner sur la page d'accueil ou voir votre panier? OK pour voir votre panier, ANNULER pour retourner sur la page d'accueil `
-            )
-          ) {
-            window.open("panier.html");
-          } else {
-            window.open("index.html");
-          }
-        };
-
         let shoppingCart = JSON.parse(localStorage.getItem("product"));
+
         /* Product in the Local Storage or not  */
 
         if (shoppingCart) {
           shoppingCart.push(selectionClient);
           localStorage.setItem("product", JSON.stringify(shoppingCart));
-          popup();
         } else {
           shoppingCart = [];
           shoppingCart.push(selectionClient);
           localStorage.setItem("product", JSON.stringify(shoppingCart));
-          popup();
         }
       });
     })
@@ -135,6 +117,30 @@ function productPage() {
 }
 
 /* -------------------------  Basket page------------------------------------------------ */
+
+// Check client
+
+let submit = document.getElementById("submit");
+let firstName = document.getElementById("inputFirstName");
+let lastName = document.getElementById("inputLastName");
+let email = document.getElementById("inputEmail");
+let address = document.getElementById("inputAddress");
+let city = document.getElementById("inputCity");
+let storage = JSON.parse(localStorage.getItem("product"));
+let validation = document.getElementById("validation");
+let inputs = document.getElementsByTagName("input");
+let error = document.getElementById("error");
+let form = document.getElementsByTagName("form");
+
+// feedback
+
+let feedBack = document.querySelector(".feedback");
+let feedbackLastName = document.querySelector(".feedbacklastname");
+let feedbackEmail = document.querySelector(".feedbackemail");
+let feedbackCity = document.querySelector(".feedbackcity");
+let feedbackAddress = document.querySelector(".feedbackaddress");
+
+/* ----------------------------------Add to the basket------------------------------------ */
 
 function basket() {
   /* Create element of the DOM */
@@ -148,8 +154,8 @@ function basket() {
       let cellTitle = document.createElement("td");
       let cellQuantity = document.createElement("td");
       let cellOption = document.createElement("td");
-      let total = document.querySelector(".total");
-      
+      let deleteButton = document.createElement("td");
+      let button = document.createElement("button");
 
       /* Add Attribute */
 
@@ -157,7 +163,8 @@ function basket() {
       cellTitle.className = "title";
       cellOption.className = "option";
       cellQuantity.className = "quantity";
-      
+      deleteButton.className = "deletebutton";
+      button.className = "btn btn-primary w-75";
 
       /* Insertion in the DOM */
 
@@ -166,6 +173,8 @@ function basket() {
       rank.appendChild(cellQuantity);
       rank.appendChild(cellOption);
       rank.appendChild(cellPrice);
+      rank.appendChild(deleteButton);
+      deleteButton.appendChild(button);
 
       /* Content of the node */
 
@@ -173,60 +182,275 @@ function basket() {
       cellQuantity.textContent = list[i].quantity;
       cellPrice.textContent = list[i].price;
       cellOption.textContent = list[i].option;
-      total.textContent = list[i].price;
+      button.textContent = "Supprimer l'article";
+
+      /* Set up button */
+
+      const productsArray = JSON.parse(localStorage.getItem("product"));
+
+      for (let i = 0; i < productsArray.length; i++) {
+        const theProduct = JSON.parse(localStorage.getItem("product"))[i];
+        button.addEventListener("click", function () {
+          let deleteRank = deleteButton.parentNode.parentNode;
+          deleteRank.removeChild(rank);
+        });
+      }
     }
 
-    /*    Send data to server   */
+    /* Sum of total */
 
-    let submit = document.getElementById("submit");
+    const productsArray = JSON.parse(localStorage.getItem("product"));
+    let totalPrice = [];
+    for (let i = 0; i < productsArray.length; i++) {
+      let priceInLocalStorage = JSON.parse(localStorage.getItem("product"))[i]
+        .price;
+      let priceInLocalStorageConvertInNum = parseInt(priceInLocalStorage);
+      totalPrice.push(priceInLocalStorageConvertInNum);
+    }
+    let total = document.querySelector(".total");
+    total.textContent = totalPrice.reduce((a, b) => a + b);
+    total.textContent += "€";
+  }
+}
+
+submit.addEventListener("click", function () {
+  const contact = {
+    firstName: firstName.value,
+    lastName: lastName.value,
+    address: address.value,
+    city: city.value,
+    email: email.value,
+  };
+
+  localStorage.setItem("contact", JSON.stringify(contact));
+  const chekname = JSON.parse(localStorage.getItem("contact")).firstName;
+  const checklastName = JSON.parse(localStorage.getItem("contact")).lastName;
+  const checkEmail = JSON.parse(localStorage.getItem("contact")).email;
+  const checkCity = JSON.parse(localStorage.getItem("contact")).city;
+  const checkAddress = JSON.parse(localStorage.getItem("contact")).address;
+
+  function checkValidity() {
+    if (/^[a-zA-Z]{3,20}$/.test(chekname)) {
+      feedBack.textContent = "données correctes";
+      return true;
+    } else {
+      feedBack.textContent = "Veuillez remplir le champ au bon format";
+
+      return false;
+    }
+  }
+
+  function checkValidityLastName() {
+    if (/^[a-zA-Z]{3,20}$/.test(checklastName)) {
+      feedbackLastName.textContent = "données correctes";
+      return true;
+    } else {
+      feedbackLastName.textContent = "Veuillez remplir le champ au bon format";
+      return false;
+    }
+  }
+
+  function checkValidityEmail() {
+    if (/^([\w-\.]+)@((?:[\w]+\.)+)([a-zA-Z]{2,4})$/i.test(checkEmail)) {
+      feedbackEmail.textContent = "données correctes";
+      return true;
+    } else {
+      feedbackEmail.textContent = "Veuillez remplir le champ au bon format";
+      return false;
+    }
+  }
+
+  function checkValidityAddress() {
+    if (/^[\w\s-ââéèçùïî]{5,30}$/i.test(checkAddress)) {
+      feedbackAddress.textContent = "données correctes";
+      return true;
+    } else {
+      feedbackAddress.textContent = "Veuillez remplir le champ au bon format";
+      return false;
+    }
+  }
+
+  function checkValidityCity() {
+    if (/^[\w\s-ââéèçùïî]{3,30}$/i.test(checkCity)) {
+      feedbackCity.textContent = "données correctes";
+      return true;
+    } else {
+      feedbackCity.textContent = "Veuillez remplir le champ au bon format";
+      return false;
+    }
+  }
+
+  if (
+    !checkValidity() ||
+    !checkValidityLastName() ||
+    !checkValidityEmail() ||
+    !checkValidityAddress() ||
+    !checkValidityCity()
+  ) {
+    localStorage.removeItem("contact");
+  } else {
+    localStorage.setItem("contact", JSON.stringify(contact));
+  }
+});
+
+/* Faire en sorte que les valeurs du formulaires restent */
+
+firstName.value = JSON.parse(localStorage.getItem("contact")).firstName;
+lastName.value = JSON.parse(localStorage.getItem("contact")).lastName;
+address.value = JSON.parse(localStorage.getItem("contact")).address;
+city.value = JSON.parse(localStorage.getItem("contact")).city;
+email.value = JSON.parse(localStorage.getItem("contact")).email;
+
+const contact = JSON.parse(localStorage.getItem("contact"));
+const productsArray = JSON.parse(localStorage.getItem("product"));
+
+const request = {
+  contact,
+  products: [],
+};
+
+for (let i = 0; i < productsArray.length; i++) {
+  const productsId = JSON.parse(localStorage.getItem("product"))[i].id;
+  request.products.push(productsId);
+}
+/* JSON.parse(localStorage.getItem("product"))[0].id */
+
+console.log("request");
+console.log(request);
+
+fetch("http://localhost:3000/api/cameras/order/", {
+  method: "POST",
+  body: JSON.stringify(request),
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+})
+  .then(function (response) {
+    if (response.ok) {
+      return response.json();
+    }
+  })
+  .then((value) => {
+    /* console.log(value); */
+    /* open("confirm.html?" + value.orderId) */
+    submit.setAttribute("formaction", "confirm.html?order=" + value.orderId);
+  });
+
+/* ------------------Confirm Order ------------------- */
+
+function confirmOrder() {
+  let confirm = document.querySelector(".confirm");
+  confirm.textContent = `Votre commande n° ${location.search.substring(
+    7
+  )} a bien été validé.\n Vous recevrez un mail de confirmation dans quelques instants`;
+  localStorage.clear();
+}
+
+/*    Send data to server   */
+
+/* let submit = document.getElementById("submit");
     let firstName = document.getElementById("inputFirstName");
     let lastName = document.getElementById("inputLastName");
     let email = document.getElementById("inputEmail");
     let address = document.getElementById("inputAddress");
     let city = document.getElementById("inputCity");
-    let test = JSON.parse(localStorage.getItem("product"));
-    
+    let storage = JSON.parse(localStorage.getItem("product"));
+    let validation = document.getElementById("validation")
+    let inputs = document.getElementsByTagName("input")
+    let error = document.getElementById("error") 
+    let form = document.getElementsByTagName("form") */
 
-    submit.addEventListener("click", function () {
-      const request = {
-        contact: {
-          firstName: firstName.value,
-          lastName: lastName.value,
-          address: address.value,
-          city: city.value,
-          email: email.value,
-        },
-        products: [],
+/* document.addEventListener("input", function () {
+      const contact = {
+        firstName: firstName.value,
+        lastName: lastName.value,
+        address: address.value,
+        city: city.value,
+        email: email.value,
       };
-      for (let i = 0; i < test.length; i++) {
-        let id = JSON.parse(localStorage.getItem("product"))[i].id;
-        request.products.push(id);
+
+      localStorage.setItem("contact", JSON.stringify(contact));
+      const chekname = JSON.parse(localStorage.getItem("contact")).firstName;
+      const checklastName = JSON.parse(
+        localStorage.getItem("contact")
+      ).lastName;
+      const checkEmail = JSON.parse(localStorage.getItem("contact")).email;
+      const checkCity = JSON.parse(localStorage.getItem("contact")).city;
+      const checkAddress = JSON.parse(localStorage.getItem("contact")).address;
+
+      function checkValidity() {
+        if (/^[a-zA-Z]{3,20}$/.test(chekname)) {
+          feedBack.textContent = "données correctes";
+          return true;
+        } else {
+          feedBack.textContent = "Veuillez remplir le champ au bon format";
+          return false;
+        }
       }
 
-      fetch("http://localhost:3000/api/cameras/order/", {
-        method: "POST",
-        body: JSON.stringify(request),
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-      })
-        .then(function (response) {
-          if (response.ok) {
-            return response.json();
-          }
-        })
-        .then((value) => {
-          submit.setAttribute("href", "confirm.html?order=" + value.orderId); 
-        });
-    });
-  }
-}
+      function checkValidityLastName() {
+        if (/^[a-zA-Z]{3,20}$/.test(checklastName)) {
+          feedbackLastName.textContent = "données correctes";
+          return true;
+        } else {
+          feedbackLastName.textContent =
+            "Veuillez remplir le champ au bon format";
+          return false;
+        }
+      }
 
-/* ------------------Confirm Order ------------------- */
+      function checkValidityEmail() {
+        if (/^([\w-\.]+)@((?:[\w]+\.)+)([a-zA-Z]{2,4})$/i.test(checkEmail)) {
+          feedbackEmail.textContent = "données correctes";
+          return true;
+        } else {
+          feedbackEmail.textContent = "Veuillez remplir le champ au bon format";
+          return false;
+        }
+      }
 
-function confirmOrder(){
-  let confirm = document.querySelector(".confirm"); 
-  confirm.textContent = `Votre commande n° ${location.search.substring(7)} a bien été validé. Vous recevrez un mail de confirmation dans quelques instants`
-  localStorage.clear();
-}
+      function checkValidityAddress() {
+        if (/^[\w\s-ââéèçùïî]{5,30}$/i.test(checkAddress)) {
+          feedbackAddress.textContent = "données correctes";
+          return true;
+        } else {
+          feedbackAddress.textContent =
+            "Veuillez remplir le champ au bon format";
+          return false;
+        }
+      }
+
+      function checkValidityCity() {
+        if (/^[\w\s-ââéèçùïî]{5,30}$/i.test(checkCity)) {
+          feedbackCity.textContent = "données correctes";
+          return true;
+        } else {
+          feedbackCity.textContent = "Veuillez remplir le champ au bon format";
+          return false;
+        }
+      }
+
+
+      if (
+        !checkValidity() ||
+        !checkValidityLastName() ||
+        !checkValidityEmail() ||
+        !checkValidityAddress() ||
+        !checkValidityCity()
+      ) {
+        localStorage.removeItem("contact");
+      } else {
+        localStorage.setItem("contact", JSON.stringify(contact));
+      }
+    }); */
+
+/* Faire en sorte que les valeurs du formulaires restent */
+
+/* firstName.value = JSON.parse(localStorage.getItem("contact")).firstName;
+    lastName.value = JSON.parse(localStorage.getItem("contact")).lastName;
+    address.value = JSON.parse(localStorage.getItem("contact")).address;
+    city.value = JSON.parse(localStorage.getItem("contact")).city;
+    email.value = JSON.parse(localStorage.getItem("contact")).email;
+ */
+/* Send data */

f8e86fb4c19152f1e0730e3705f0dd3cf05cd542
Yohan Etifier <etifier.y@gmail.com>
2021-05-27 11:31:49 +0200

    update basket page with dynamic table and counter set up

--- index.js
+++ index.js
@@ -141,11 +141,63 @@ let feedbackCity = document.querySelector(".feedbackcity");
 let feedbackAddress = document.querySelector(".feedbackaddress");
 
 /* ----------------------------------Add to the basket------------------------------------ */
+const productsArray = JSON.parse(localStorage.getItem("product"));
 
+function counter(){
+  let counter = document.querySelector(".counter")
+  if (productsArray.length > 0)
+  
+  counter.textContent = JSON.parse(localStorage.product).length; 
+}
 function basket() {
-  /* Create element of the DOM */
 
-  if (localStorage.product) {
+  const productsArray = JSON.parse(localStorage.getItem("product"));
+  let header = document.querySelector(".header");
+  let tfoot = document.querySelector(".footer");
+
+  if (productsArray.length > 0) {
+  /* Create the title of the table */
+
+    let thTitle = document.createElement("th")
+    let thPrice = document.createElement("th")
+    let thQuantity = document.createElement("th")
+    let thOption = document.createElement("th")
+    let thDelete = document.createElement("th")
+    let thTotal = document.createElement("th")
+    let total = document.createElement("td")
+
+  /* Add attribute to the title of the table */
+
+  thTitle.setAttribute("scope","col"); 
+  thPrice.setAttribute("scope","col");
+  thQuantity.setAttribute("scope","col");
+  thOption.setAttribute("scope","col");
+  thDelete.setAttribute("scope","col");
+  thTotal.setAttribute("colspan","3");
+  total.className = "total"
+
+  /* Add textContent to the title */
+
+  thTitle.textContent = "Libellé"
+  thPrice.textContent = "Prix"
+  thQuantity.textContent = "Quantité"
+  thOption.textContent = "Option"
+  thDelete.textContent = "Supprimer"
+  thTotal.textContent = "Total"
+  
+
+  /* Insertion in the dom */
+
+  header.appendChild(thTitle)
+  header.appendChild(thQuantity)
+  header.appendChild(thOption)
+  header.appendChild(thPrice)
+  header.appendChild(thDelete)
+  tfoot.appendChild(thTotal)
+  tfoot.appendChild(total)
+
+
+
     for (let i = 0; i < JSON.parse(localStorage.product).length; i++) {
       let list = JSON.parse(localStorage.getItem("product"));
       let tbody = document.querySelector(".tbody");
@@ -187,155 +239,197 @@ function basket() {
       /* Set up button */
 
       const productsArray = JSON.parse(localStorage.getItem("product"));
+      const productsInTheLocalStorage = JSON.stringify(productsArray);
+      const theProduct = JSON.parse(localStorage.getItem("product"))[i];
 
-      for (let i = 0; i < productsArray.length; i++) {
-        const theProduct = JSON.parse(localStorage.getItem("product"))[i];
-        button.addEventListener("click", function () {
-          let deleteRank = deleteButton.parentNode.parentNode;
-          deleteRank.removeChild(rank);
-        });
-      }
+      button.addEventListener("click", function () {
+        let deleteRank = deleteButton.parentNode.parentNode;
+        deleteRank.removeChild(rank);
+        productsArray.splice(i, 1);
+        localStorage.setItem("product", JSON.stringify(productsArray));
+        /* console.log("productsArray")
+          console.log(productsArray)
+          console.log("productsArray[i]")
+          console.log(productsArray[i]) */
+        /* console.log("test")
+          console.log(test) */
+        /*  console.log("productsArray")
+          console.log(productsArray) */
+        window.location.href = "panier.html";
+        /* console.log("productsArray[i]")
+          console.log(productsArray[i]) */
+
+        /* productsArray.splice(theProduct,i)
+          localStorage.setItem("product", JSON.stringify(productsArray))
+          console.log("theProduct")
+          console.log(theProduct)
+          console.log("productsArray")
+          console.log(productsArray)
+          console.log("productsInTheLocalStorage")
+          console.log(productsInTheLocalStorage) */
+      });
     }
 
     /* Sum of total */
 
     const productsArray = JSON.parse(localStorage.getItem("product"));
-    let totalPrice = [];
-    for (let i = 0; i < productsArray.length; i++) {
-      let priceInLocalStorage = JSON.parse(localStorage.getItem("product"))[i]
-        .price;
-      let priceInLocalStorageConvertInNum = parseInt(priceInLocalStorage);
-      totalPrice.push(priceInLocalStorageConvertInNum);
+    if (productsArray.length > 0) {
+      let totalPrice = [];
+      for (let i = 0; i < productsArray.length; i++) {
+        let priceInLocalStorage = JSON.parse(localStorage.getItem("product"))[i]
+          .price;
+        let priceInLocalStorageConvertInNum = parseInt(priceInLocalStorage);
+        totalPrice.push(priceInLocalStorageConvertInNum);
+      }
+      let total = document.querySelector(".total");
+      total.textContent = totalPrice.reduce((a, b) => a + b);
+      total.textContent += "€";
     }
-    let total = document.querySelector(".total");
-    total.textContent = totalPrice.reduce((a, b) => a + b);
-    total.textContent += "€";
+
+    /* When a user click */
+
+    submit.addEventListener("click", function () {
+      /* Make a object with the infoclient */
+      const infoClient = {
+        firstName: firstName.value,
+        lastName: lastName.value,
+        address: address.value,
+        city: city.value,
+        email: email.value,
+      };
+
+      /* Create the object in the local storage */
+
+      localStorage.setItem("contact", JSON.stringify(infoClient));
+      const chekname = JSON.parse(localStorage.getItem("contact")).firstName;
+      const checklastName = JSON.parse(
+        localStorage.getItem("contact")
+      ).lastName;
+      const checkEmail = JSON.parse(localStorage.getItem("contact")).email;
+      const checkCity = JSON.parse(localStorage.getItem("contact")).city;
+      const checkAddress = JSON.parse(localStorage.getItem("contact")).address;
+
+      /* Control user input */
+
+      function checkValidity() {
+        if (/^[a-zA-Z]{3,20}$/.test(chekname)) {
+          feedBack.textContent = "données correctes";
+          return true;
+        } else {
+          feedBack.textContent = "Veuillez remplir le champ au bon format";
+
+          return false;
+        }
+      }
+
+      function checkValidityLastName() {
+        if (/^[a-zA-Z]{3,20}$/.test(checklastName)) {
+          feedbackLastName.textContent = "données correctes";
+          return true;
+        } else {
+          feedbackLastName.textContent =
+            "Veuillez remplir le champ au bon format";
+          return false;
+        }
+      }
+
+      function checkValidityEmail() {
+        if (/^([\w-\.]+)@((?:[\w]+\.)+)([a-zA-Z]{2,4})$/i.test(checkEmail)) {
+          feedbackEmail.textContent = "données correctes";
+          return true;
+        } else {
+          feedbackEmail.textContent = "Veuillez remplir le champ au bon format";
+          return false;
+        }
+      }
+
+      function checkValidityAddress() {
+        if (/^[\w\s-ââéèçùïî]{5,30}$/i.test(checkAddress)) {
+          feedbackAddress.textContent = "données correctes";
+          return true;
+        } else {
+          feedbackAddress.textContent =
+            "Veuillez remplir le champ au bon format";
+          return false;
+        }
+      }
+
+      function checkValidityCity() {
+        if (/^[\w\s-ââéèçùïî]{3,30}$/i.test(checkCity)) {
+          feedbackCity.textContent = "données correctes";
+          return true;
+        } else {
+          feedbackCity.textContent = "Veuillez remplir le champ au bon format";
+          return false;
+        }
+      }
+
+      if (
+        !checkValidity() ||
+        !checkValidityLastName() ||
+        !checkValidityEmail() ||
+        !checkValidityAddress() ||
+        !checkValidityCity()
+      ) {
+        localStorage.removeItem("contact");
+      } else {
+        localStorage.setItem("contact", JSON.stringify(infoClient));
+      }
+    });
+
+    /* Faire en sorte que les valeurs du formulaires restent */
+
+    if (localStorage.getItem("contact")) {
+      firstName.value = JSON.parse(localStorage.getItem("contact")).firstName;
+      lastName.value = JSON.parse(localStorage.getItem("contact")).lastName;
+      address.value = JSON.parse(localStorage.getItem("contact")).address;
+      city.value = JSON.parse(localStorage.getItem("contact")).city;
+      email.value = JSON.parse(localStorage.getItem("contact")).email;
+    }
+
+    /* Create object for the server */
+
+    const contact = JSON.parse(localStorage.getItem("contact"));
+    const productsArr = JSON.parse(localStorage.getItem("product"));
+
+    const request = {
+      contact,
+      products: [],
+    };
+
+    /* Put all the products ID in the object */
+
+    for (let i = 0; i < productsArr.length; i++) {
+      const productsId = JSON.parse(localStorage.getItem("product"))[i].id;
+      request.products.push(productsId);
+    }
+    /* JSON.parse(localStorage.getItem("product"))[0].id */
+
+    /* Send the object to the server */
+
+    if (localStorage.product && localStorage.contact) {
+      fetch("http://localhost:3000/api/cameras/order/", {
+        method: "POST",
+        body: JSON.stringify(request),
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      })
+        .then(function (response) {
+          if (response.ok) {
+            return response.json();
+          }
+        })
+        .then((value) => {
+          window.location.href = "confirm.html?order=" + value.orderId;
+        });
+    }
+  }else {
+    let infoPurchase = document.querySelector(".infopurchase")
+    infoPurchase.textContent = "Votre panier est vide"
   }
 }
-
-submit.addEventListener("click", function () {
-  const contact = {
-    firstName: firstName.value,
-    lastName: lastName.value,
-    address: address.value,
-    city: city.value,
-    email: email.value,
-  };
-
-  localStorage.setItem("contact", JSON.stringify(contact));
-  const chekname = JSON.parse(localStorage.getItem("contact")).firstName;
-  const checklastName = JSON.parse(localStorage.getItem("contact")).lastName;
-  const checkEmail = JSON.parse(localStorage.getItem("contact")).email;
-  const checkCity = JSON.parse(localStorage.getItem("contact")).city;
-  const checkAddress = JSON.parse(localStorage.getItem("contact")).address;
-
-  function checkValidity() {
-    if (/^[a-zA-Z]{3,20}$/.test(chekname)) {
-      feedBack.textContent = "données correctes";
-      return true;
-    } else {
-      feedBack.textContent = "Veuillez remplir le champ au bon format";
-
-      return false;
-    }
-  }
-
-  function checkValidityLastName() {
-    if (/^[a-zA-Z]{3,20}$/.test(checklastName)) {
-      feedbackLastName.textContent = "données correctes";
-      return true;
-    } else {
-      feedbackLastName.textContent = "Veuillez remplir le champ au bon format";
-      return false;
-    }
-  }
-
-  function checkValidityEmail() {
-    if (/^([\w-\.]+)@((?:[\w]+\.)+)([a-zA-Z]{2,4})$/i.test(checkEmail)) {
-      feedbackEmail.textContent = "données correctes";
-      return true;
-    } else {
-      feedbackEmail.textContent = "Veuillez remplir le champ au bon format";
-      return false;
-    }
-  }
-
-  function checkValidityAddress() {
-    if (/^[\w\s-ââéèçùïî]{5,30}$/i.test(checkAddress)) {
-      feedbackAddress.textContent = "données correctes";
-      return true;
-    } else {
-      feedbackAddress.textContent = "Veuillez remplir le champ au bon format";
-      return false;
-    }
-  }
-
-  function checkValidityCity() {
-    if (/^[\w\s-ââéèçùïî]{3,30}$/i.test(checkCity)) {
-      feedbackCity.textContent = "données correctes";
-      return true;
-    } else {
-      feedbackCity.textContent = "Veuillez remplir le champ au bon format";
-      return false;
-    }
-  }
-
-  if (
-    !checkValidity() ||
-    !checkValidityLastName() ||
-    !checkValidityEmail() ||
-    !checkValidityAddress() ||
-    !checkValidityCity()
-  ) {
-    localStorage.removeItem("contact");
-  } else {
-    localStorage.setItem("contact", JSON.stringify(contact));
-  }
-});
-
-/* Faire en sorte que les valeurs du formulaires restent */
-
-firstName.value = JSON.parse(localStorage.getItem("contact")).firstName;
-lastName.value = JSON.parse(localStorage.getItem("contact")).lastName;
-address.value = JSON.parse(localStorage.getItem("contact")).address;
-city.value = JSON.parse(localStorage.getItem("contact")).city;
-email.value = JSON.parse(localStorage.getItem("contact")).email;
-
-const contact = JSON.parse(localStorage.getItem("contact"));
-const productsArray = JSON.parse(localStorage.getItem("product"));
-
-const request = {
-  contact,
-  products: [],
-};
-
-for (let i = 0; i < productsArray.length; i++) {
-  const productsId = JSON.parse(localStorage.getItem("product"))[i].id;
-  request.products.push(productsId);
-}
-/* JSON.parse(localStorage.getItem("product"))[0].id */
-
-console.log("request");
-console.log(request);
-
-fetch("http://localhost:3000/api/cameras/order/", {
-  method: "POST",
-  body: JSON.stringify(request),
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json",
-  },
-})
-  .then(function (response) {
-    if (response.ok) {
-      return response.json();
-    }
-  })
-  .then((value) => {
-    /* console.log(value); */
-    /* open("confirm.html?" + value.orderId) */
-    submit.setAttribute("formaction", "confirm.html?order=" + value.orderId);
-  });
 
 /* ------------------Confirm Order ------------------- */
 

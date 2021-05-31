840b06cd1044c053be887c9426e5673bafce8246
Yohan Etifier <etifier.y@gmail.com>
2021-05-31 19:47:17 +0200

    search bar

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function getProduct() {
         let description = document.createElement("p");
         let link = document.createElement("a");
         let container = document.querySelector(".product");
-        let icons = `<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
+        let icons = `<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
         <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
       </svg>`
 
@@ -49,7 +49,7 @@ function getProduct() {
         cardBody.appendChild(title);
         cardBody.appendChild(description);
         cardBody.appendChild(link);
-        link.innerHTML = icons + "pour plus de détails"
+        link.innerHTML = icons + "Voir plus"
       })
       .catch(function (err) {
         console.log("une erreur est survenue");
